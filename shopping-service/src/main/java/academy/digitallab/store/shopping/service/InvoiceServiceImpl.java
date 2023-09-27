203b0004fd02ee086c2c86624f72a2150b979f07
Sergi <sdiaz@isatechdata.com>
2023-09-27 11:55:06 +0200

    comunicacion entre microservicios

--- shopping-service/src/main/java/academy/digitallab/store/shopping/service/InvoiceServiceImpl.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/service/InvoiceServiceImpl.java
@@ -1,10 +1,10 @@
 package academy.digitallab.store.shopping.service;
 
-//import academy.digitallab.store.shopping.client.CustomerClient;
-//import academy.digitallab.store.shopping.client.ProductClient;
-//import academy.digitallab.store.shopping.entity.InvoiceItem;
-//import academy.digitallab.store.shopping.model.Customer;
-//import academy.digitallab.store.shopping.model.Product;
+import academy.digitallab.store.shopping.client.CustomerClient;
+import academy.digitallab.store.shopping.client.ProductClient;
+import academy.digitallab.store.shopping.entity.InvoiceItem;
+import academy.digitallab.store.shopping.model.Customer;
+import academy.digitallab.store.shopping.model.Product;
 import academy.digitallab.store.shopping.repository.InvoiceItemsRepository;
 import academy.digitallab.store.shopping.repository.InvoiceRepository;
 import academy.digitallab.store.shopping.entity.Invoice;
@@ -25,11 +25,11 @@ public class InvoiceServiceImpl implements InvoiceService {
 
     @Autowired
     InvoiceItemsRepository invoiceItemsRepository;
-//    @Autowired
-//    CustomerClient customerClient;
-//
-//    @Autowired
-//    ProductClient productClient;
+    @Autowired
+    CustomerClient customerClient;
+
+    @Autowired
+    ProductClient productClient;
 
     @Override
     public List<Invoice> findInvoiceAll() {
@@ -45,9 +45,9 @@ public class InvoiceServiceImpl implements InvoiceService {
         }
         invoice.setState("CREATED");
         invoiceDB = invoiceRepository.save(invoice);
-//        invoiceDB.getItems().forEach( invoiceItem -> {
-//            productClient.updateStockProduct( invoiceItem.getProductId(), invoiceItem.getQuantity() * -1);
-//        });
+        invoiceDB.getItems().forEach( invoiceItem -> {
+            productClient.updateStockProduct( invoiceItem.getProductId(), invoiceItem.getQuantity() * (-1));
+        });
 
         return invoiceDB;
     }
@@ -80,16 +80,17 @@ public class InvoiceServiceImpl implements InvoiceService {
 
     @Override
     public Invoice getInvoice(Long id) {
+
         Invoice invoice= invoiceRepository.findById(id).orElse(null);
         if (null != invoice ){
-//            Customer customer = customerClient.getCustomer(invoice.getCustomerId()).getBody();
-//            invoice.setCustomer(customer);
-//            List<InvoiceItem> listItem=invoice.getItems().stream().map(invoiceItem -> {
-//                Product product = productClient.getProduct(invoiceItem.getProductId()).getBody();
-//                invoiceItem.setProduct(product);
-//                return invoiceItem;
-//            }).collect(Collectors.toList());
-//            invoice.setItems(listItem);
+            Customer customer = customerClient.getCustomer(invoice.getCustomerId()).getBody();
+            invoice.setCustomer(customer);
+            List<InvoiceItem> listItem=invoice.getItems().stream().map(invoiceItem -> {
+                Product product = productClient.getProduct(invoiceItem.getProductId()).getBody();
+                invoiceItem.setProduct(product);
+                return invoiceItem;
+            }).collect(Collectors.toList());
+            invoice.setItems(listItem);
         }
         return invoice ;
     }
